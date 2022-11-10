22f8221d1a2f400b329b4a6719bc8d6531efb39b
anon <anon@anon.ymous>
2022-11-09 23:56:09 -0800

    ipgeo null fix

--- ipgeo.sh
+++ ipgeo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-curl -s https://ipinfo.io | jq -rM '.ip,.hostname,"\(.city), \(.region), \(.country)",.org'
+curl -s https://ipinfo.io | jq -rM '.ip,.hostname // "No Reverse DNS","\(.city), \(.region), \(.country)",.org'
 
